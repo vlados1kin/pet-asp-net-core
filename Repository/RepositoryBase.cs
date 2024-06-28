8fa2e49b6c0bf0812e49c98bd5d5f2f2a40172fa
Vladislav Lashkin <lashkin2004@gmail.com>
2024-06-28 21:43:03 +0300

    Update: implement repository base contract

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+    private readonly RepositoryContext _repositoryContext;
+
+    public RepositoryBase(RepositoryContext repositoryContext) => _repositoryContext = repositoryContext;
+
+    public IQueryable<T> FindAll(bool trackChanges) =>
+        !trackChanges
+            ? _repositoryContext.Set<T>().AsNoTracking()
+            : _repositoryContext.Set<T>();
+
+    public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+        !trackChanges
+            ? _repositoryContext.Set<T>().Where(expression).AsNoTracking()
+            : _repositoryContext.Set<T>().Where(expression);
+
+    public void Create(T entity) => _repositoryContext.Set<T>().Add(entity);
+    public void Update(T entity) => _repositoryContext.Set<T>().Update(entity);
+    public void Delete(T entity) => _repositoryContext.Set<T>().Remove(entity);
+}
