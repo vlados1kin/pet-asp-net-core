1324974cc42680a541623bcc91e3e2c233f9f2df
Vladislav Lashkin <lashkin2004@gmail.com>
2024-06-30 11:21:10 +0300

    Update: add mapper to solution

--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -1,4 +1,5 @@
-﻿using Contracts;
+﻿using AutoMapper;
+using Contracts;
 using Service.Contracts;
 
 namespace Service;
@@ -8,12 +9,12 @@ public sealed class ServiceManager : IServiceManager
     private readonly Lazy<ICompanyService> _companyService;
     private readonly Lazy<IEmployeeService> _employeeService;
 
-    public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger)
+    public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger, IMapper mapper)
     {
         _companyService = new Lazy<ICompanyService>(() => new
-            CompanyService(repositoryManager, logger));
+            CompanyService(repositoryManager, logger, mapper));
         _employeeService = new Lazy<IEmployeeService>(() => new
-            EmployeeService(repositoryManager, logger));
+            EmployeeService(repositoryManager, logger, mapper));
     }
 
     public ICompanyService CompanyService => _companyService.Value;
