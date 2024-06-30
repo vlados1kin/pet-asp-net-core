b8eb341ba64ff48aa6d923a8db855589bd983228
Vladislav Lashkin <lashkin2004@gmail.com>
2024-06-30 11:21:54 +0300

    Add: new mapping profile

---
+++ CompanyEmployees/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Entities.Models;
+using Shared.DataTransferObject;
+
+namespace UltimateASPNETApp;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // CreateMap<Company, CompanyDto>().ForMember(c => c.FullAddress,
+        //     options => options.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+        CreateMap<Company, CompanyDto>()
+            .ForCtorParam("FullAddress", options => options.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+    }
+}
