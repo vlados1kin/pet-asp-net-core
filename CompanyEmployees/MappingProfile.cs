0710a3fc3dd88154853834aaccbbb097e7cca894
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-04 00:41:41 +0300

    Add: new employee for company action

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -16,5 +16,7 @@ public class MappingProfile : Profile
         CreateMap<Employee, EmployeeDto>();
         
         CreateMap<CompanyForCreationDto, Company>();
+        
+        CreateMap<EmployeeForCreationDto, Employee>();
     }
 }
--- Presentation/Controllers/EmployeesController.cs
+++ Presentation/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
+using Shared.DataTransferObject;
 
 namespace Presentation.Controllers;
 
@@ -18,11 +19,20 @@ public class EmployeesController : ControllerBase
         return Ok(employees);
     }
 
-    [HttpGet("{id:guid}")]
+    [HttpGet("{id:guid}", Name = "GetEmployeeForCompany")]
     public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
     {
         var employee = _service.EmployeeService.GetEmployee(companyId, id, trackChanges: false);
         return Ok(employee);
     }
+
+    [HttpPost]
+    public IActionResult CreateEmployeeForCompany(Guid companyId, [FromBody] EmployeeForCreationDto employee)
+    {
+        if (employee is null)
+            return BadRequest("EmployeeForCreationDto object is null");
+        var employeeToReturn =
+            _service.EmployeeService.CreateEmployeeForCompany(companyId, employee, trackChanges: false);
+        return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = employeeToReturn.Id }, employeeToReturn);
+    }
 }
-    
