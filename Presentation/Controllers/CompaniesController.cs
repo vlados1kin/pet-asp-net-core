0d278746810b99fc545435707348ec5413bdc7f2
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-04 22:40:13 +0300

    Add: new collection action

--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -9,7 +9,7 @@ namespace Presentation.Controllers;
 public class CompaniesController : ControllerBase
 {
     private readonly IServiceManager _service;
-    
+
     public CompaniesController(IServiceManager service) => _service = service;
 
     [HttpGet]
@@ -25,18 +25,31 @@ public class CompaniesController : ControllerBase
         var company = _service.CompanyService.GetCompany(id, trackChanges: false);
         return Ok(company);
     }
-    
+
     [HttpPost]
     public IActionResult CreateCompany([FromBody] CompanyForCreationDto company)
     {
         if (company is null)
         {
-            return BadRequest("CompanyForCreationDto object is null");   
+            return BadRequest("CompanyForCreationDto object is null");
         }
-        
+
         var createdCompany = _service.CompanyService.CreateCompany(company);
-        
+
         return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
     }
 
+    [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+    public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+    {
+        var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+        return Ok(companies);
+    }
+
+    [HttpPost("collection")]
+    public IActionResult CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companyCollection)
+    {
+        var result = _service.CompanyService.CreateCompanyCollection(companyCollection);
+        return CreatedAtRoute("CompanyCollection", new { result.ids }, result.companies);
+    }
 }
--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -17,4 +17,7 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
         FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
 
     public void CreateCompany(Company company) => Create(company);
+
+    public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+        FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
 }
