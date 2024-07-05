908609bf19ce52eeaabed3202d711e949eb0a508
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-05 13:21:05 +0300

    Add: new delete action

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -54,4 +54,16 @@ public sealed class EmployeeService : IEmployeeService
         var employeeToReturn = _mapper.Map<EmployeeDto>(employeeEntity);
         return employeeToReturn;
     }
+
+    public void DeleteEmployeeForCompany(Guid companyId, Guid id, bool trackChanges)
+    {
+        var company = _repository.Company.GetCompany(companyId, trackChanges);
+        if (company is null)
+            throw new CompanyNotFoundException(companyId);
+        var employee = _repository.Employee.GetEmployee(companyId, id, trackChanges);
+        if (employee is null)
+            throw new EmployeeNotFoundException(id);
+        _repository.Employee.DeleteEmployee(employee);
+        _repository.Save();
+    }
 }
