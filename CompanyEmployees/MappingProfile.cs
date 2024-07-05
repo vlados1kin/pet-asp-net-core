6837c69f421dd3f870931d0dc7e0dbf33749a689
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-05 21:34:37 +0300

    Add: new company for update action

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -20,5 +20,7 @@ public class MappingProfile : Profile
         CreateMap<EmployeeForCreationDto, Employee>();
         
         CreateMap<EmployeeForUpdateDto, Employee>();
+        
+        CreateMap<CompanyForUpdateDto, Company>();
     }
 }
--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -61,4 +61,13 @@ public class CompaniesController : ControllerBase
         _service.CompanyService.DeleteCompany(id, trackChanges: false);
         return NoContent();
     }
+    
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company)
+    {
+        if (company is null)
+            return BadRequest("CompanyForUpdateDto object is null");
+        _service.CompanyService.UpdateCompany(id, company, trackChanges: true);
+        return NoContent();
+    }
 }
