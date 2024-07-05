436ec690155db717fba55cce8ae5226742f2134c
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-05 12:50:51 +0300

    Add: connect array model binder to project

--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.ModelBinders;
 using Service.Contracts;
 using Shared.DataTransferObject;
 
@@ -40,7 +41,8 @@ public class CompaniesController : ControllerBase
     }
 
     [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-    public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+    public IActionResult GetCompanyCollection(
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
     {
         var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
         return Ok(companies);
