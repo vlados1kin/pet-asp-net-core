6570e784045efbb8265fdccbe9e3f0123f7c4250
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-07 16:42:54 +0300

    Add: new validation for employee dto

--- Presentation/Controllers/EmployeesController.cs
+++ Presentation/Controllers/EmployeesController.cs
@@ -32,6 +32,8 @@ public class EmployeesController : ControllerBase
     {
         if (employee is null)
             return BadRequest("EmployeeForCreationDto object is null");
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
         var employeeToReturn =
             _service.EmployeeService.CreateEmployeeForCompany(companyId, employee, trackChanges: false);
         return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = employeeToReturn.Id }, employeeToReturn);
--- Shared/DataTransferObject/EmployeeForCreationDto.cs
+++ Shared/DataTransferObject/EmployeeForCreationDto.cs
@@ -1,3 +1,17 @@
-﻿namespace Shared.DataTransferObject;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record EmployeeForCreationDto(string Name, int Age, string Position);
+namespace Shared.DataTransferObject;
+
+public record EmployeeForCreationDto
+{
+    [Required(ErrorMessage = "Employee name is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+    public string? Name { get; init; }
+
+    [Required(ErrorMessage = "Age is a required field.")]
+    public int Age { get; init; }
+
+    [Required(ErrorMessage = "Position is a required field.")]
+    [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+    public string? Position { get; init; }
+}
