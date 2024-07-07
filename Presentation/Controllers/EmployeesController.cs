eaba13f8daeb909ef1cb375063d920b5d97c54da
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-07 23:56:01 +0300

    Edit: add validation to patch method

--- Presentation/Controllers/EmployeesController.cs
+++ Presentation/Controllers/EmployeesController.cs
@@ -51,6 +51,8 @@ public class EmployeesController : ControllerBase
     {
         if (employee is null)
             return BadRequest("EmployeeForUpdateDto object is null");
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
         _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee, compTrackChanges: false,
             empTrackChanges: true);
         return NoContent();
@@ -64,7 +66,10 @@ public class EmployeesController : ControllerBase
             return BadRequest("patchDoc object sent from client is null.");
         var result =
             _service.EmployeeService.GetEmployeeForPatch(companyId, id, compTrackChanges: false, empTrackChanges: true);
-        patchDoc.ApplyTo(result.employeeToPatch);
+        patchDoc.ApplyTo(result.employeeToPatch, ModelState);
+        TryValidateModel(result.employeeToPatch);
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
         _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
         return NoContent();
     }
