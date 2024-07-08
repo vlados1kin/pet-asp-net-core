278dda18a9ee3f08f7f89f687eeddb5cd5e6aa10
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-09 00:00:02 +0300

    Edit: restore csv formatter

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -10,7 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
-
+NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter() =>
+    new ServiceCollection()
+        .AddLogging()
+        .AddMvc()
+        .AddNewtonsoftJson().Services
+        .BuildServiceProvider()
+        .GetRequiredService<IOptions<MvcOptions>>().Value.InputFormatters
+        .OfType<NewtonsoftJsonPatchInputFormatter>().First();
 
 
 builder.Services.AddAutoMapper(typeof(Program));
@@ -22,6 +29,7 @@ builder.Services.AddControllers(config =>
     {
         config.RespectBrowserAcceptHeader = true;
         config.ReturnHttpNotAcceptable = true;
+        config.InputFormatters.Insert(0, GetJsonPatchInputFormatter());
     })
     .AddXmlDataContractSerializerFormatters()
     .AddCustomCSVFormatter()
