b5487e111706c20b258620a5f09b1588b9bf9f87
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-09 23:46:29 +0300

    Edit: implement new async methods

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository;
 
@@ -10,16 +11,16 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     {
     }
 
-    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
-        FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+    public async Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges) =>
+        await FindAll(trackChanges).OrderBy(c => c.Name).ToListAsync();
 
-    public Company GetCompany(Guid companyId, bool trackChanges) =>
-        FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
+    public async Task<Company> GetCompanyAsync(Guid companyId, bool trackChanges) =>
+        await FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefaultAsync();
 
     public void CreateCompany(Company company) => Create(company);
 
-    public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
-        FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
+    public async Task<IEnumerable<Company>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges) =>
+        await FindByCondition(x => ids.Contains(x.Id), trackChanges).ToListAsync();
 
     public void DeleteCompany(Company company) => Delete(company);
 }
