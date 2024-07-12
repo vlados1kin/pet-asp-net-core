f13ee309abffdf3b82cb9cee9b21dea10046fb58
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-12 15:07:53 +0300

    Add: new parameter class

---
+++ Shared/RequestFeatures/RequestParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.RequestFeatures;
+
+public abstract class RequestParameters
+{
+    private const int MaxPageSize = 50;
+    private int _pageSize = 10;
+
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+}
