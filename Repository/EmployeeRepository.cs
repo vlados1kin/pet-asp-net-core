5176c92b88923a27d395274ac696dd4e94212d6a
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-12 19:42:19 +0300

    Edit: enhance performance in get employees method

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -16,10 +16,12 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     {
         var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
             .OrderBy(e => e.Name)
+            .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
+            .Take(employeeParameters.PageSize)
             .ToListAsync();
-        return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
+        var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
+        return new PagedList<Employee>(employees, count, employeeParameters.PageNumber, employeeParameters.PageSize);
     }
-
 
     public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
         await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
