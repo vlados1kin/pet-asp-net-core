695cc9a03f1e9b10d04883ab1790825247b11044
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-12 18:09:46 +0300

    Edit: refactor service with paged list

--- Contracts/IEmployeeRepository.cs
+++ Contracts/IEmployeeRepository.cs
@@ -5,9 +5,8 @@ namespace Contracts;
 
 public interface IEmployeeRepository
 {
-    Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters,
-        bool trackChanges);
-
+    Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId,
+        EmployeeParameters employeeParameters, bool trackChanges);
     Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges);
     void CreateEmployeeForCompany(Guid companyId, Employee employee);
     void DeleteEmployee(Employee employee);
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -7,22 +7,23 @@ namespace Repository;
 
 public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
 {
-    public EmployeeRepository(RepositoryContext repositoryContext)
-        : base(repositoryContext)
+    public EmployeeRepository(RepositoryContext repositoryContext) : base(repositoryContext)
     {
     }
 
-    public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, 
-        EmployeeParameters employeeParameters, bool trackChanges) =>
-        await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+    public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters,
+        bool trackChanges)
+    {
+        var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
             .OrderBy(e => e.Name)
-            .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
-            .Take(employeeParameters.PageSize)
             .ToListAsync();
+        return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
+    }
 
 
     public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
-        await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges).SingleOrDefaultAsync();
+        await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
+            .SingleOrDefaultAsync();
 
     public void CreateEmployeeForCompany(Guid companyId, Employee employee)
     {
