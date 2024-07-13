5c3dfec821fd4a1990dc0c472e3c9dec367a24d6
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-13 13:48:31 +0300

    Edit: allow filtering in get method

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -11,16 +11,15 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     {
     }
 
-    public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters,
-        bool trackChanges)
+    public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId,
+        EmployeeParameters employeeParameters, bool trackChanges)
     {
-        var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+        var employees = await FindByCondition(
+                e => e.CompanyId.Equals(companyId) &&
+                     (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
             .OrderBy(e => e.Name)
-            .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
-            .Take(employeeParameters.PageSize)
             .ToListAsync();
-        var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
-        return new PagedList<Employee>(employees, count, employeeParameters.PageNumber, employeeParameters.PageSize);
+        return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
     }
 
     public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
