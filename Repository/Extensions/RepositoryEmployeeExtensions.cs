0417aaadc391687d54f60d9330ca731e6ccf7cc0
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-14 19:38:20 +0300

    Add: new order by method in extension

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Entities.Models;
+﻿using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
+using Entities.Models;
 
 namespace Repository.Extensions;
 
@@ -13,5 +16,29 @@ public static class RepositoryEmployeeExtensions
             return employees;
         var lowerCaseTerm = searchTerm.Trim().ToLower();
         return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+    }
+    
+    public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+    {
+        if (string.IsNullOrWhiteSpace(orderByQueryString))
+            return employees.OrderBy(e => e.Name);
+        var orderParams = orderByQueryString.Trim().Split(',');
+        var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var orderQueryBuilder = new StringBuilder();
+        foreach (var param in orderParams)
+        {
+            if (string.IsNullOrWhiteSpace(param))
+                continue;
+            var propertyFromQueryName = param.Split(" ")[0];
+            var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+            if (objectProperty == null)
+                continue;
+            var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+        }
+        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+        if (string.IsNullOrWhiteSpace(orderQuery))
+            return employees.OrderBy(e => e.Name);
+        return employees.OrderBy(orderQuery);
     }
 }
