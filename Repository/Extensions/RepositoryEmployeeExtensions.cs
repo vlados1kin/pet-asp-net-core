f336d4d0304b9c65fa3715bd8cd4088bd068a88a
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-15 00:06:48 +0300

    Edit: refactor sort method a bit

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Text;
 using Entities.Models;
+using Repository.Extensions.Utility;
 
 namespace Repository.Extensions;
 
@@ -22,21 +23,25 @@ public static class RepositoryEmployeeExtensions
     {
         if (string.IsNullOrWhiteSpace(orderByQueryString))
             return employees.OrderBy(e => e.Name);
-        var orderParams = orderByQueryString.Trim().Split(',');
-        var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-        var orderQueryBuilder = new StringBuilder();
-        foreach (var param in orderParams)
-        {
-            if (string.IsNullOrWhiteSpace(param))
-                continue;
-            var propertyFromQueryName = param.Split(" ")[0];
-            var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-            if (objectProperty == null)
-                continue;
-            var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
-        }
-        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+        
+        var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+        
+        // var orderParams = orderByQueryString.Trim().Split(',');
+        // var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        // var orderQueryBuilder = new StringBuilder();
+        // foreach (var param in orderParams)
+        // {
+        //     if (string.IsNullOrWhiteSpace(param))
+        //         continue;
+        //     var propertyFromQueryName = param.Split(" ")[0];
+        //     var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+        //     if (objectProperty == null)
+        //         continue;
+        //     var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+        //     orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+        // }
+        // var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+        
         if (string.IsNullOrWhiteSpace(orderQuery))
             return employees.OrderBy(e => e.Name);
         return employees.OrderBy(orderQuery);
