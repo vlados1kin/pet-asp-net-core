008f46f1023bcb6c1431df2bbb485640303cc794
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-17 23:44:18 +0300

    Edit: register employee links dependence

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Dynamic;
+using Entities.LinkModels;
 using Entities.Models;
 using Shared.DataTransferObject;
 using Shared.RequestFeatures;
@@ -7,8 +8,8 @@ namespace Service.Contracts;
 
 public interface IEmployeeService
 {
-    Task<(IEnumerable<Entity> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId,
-        EmployeeParameters employeeParameters, bool trackChanges);
+    Task<(LinkResponse linkResponse, MetaData metaData)> GetEmployeesAsync(Guid companyId,
+        LinkParameters linkParameters, bool trackChanges);
 
     Task<EmployeeDto> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges);
 
