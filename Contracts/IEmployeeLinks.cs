b629f6993b172030657172d825a79b3bff8e1411
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-17 23:43:13 +0300

    Add: new employee links class

---
+++ Contracts/IEmployeeLinks.cs
@@ -0,0 +1,10 @@
+﻿using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
+using Shared.DataTransferObject;
+
+namespace Contracts;
+
+public interface IEmployeeLinks
+{
+    LinkResponse TryGenerateLinks(IEnumerable<EmployeeDto> employeesDto, string fields, Guid companyId, HttpContext httpContext);
+}
