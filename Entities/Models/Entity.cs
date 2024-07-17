b97f7cbca14637299d1aa0699f0fb08c02dbdc71
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-17 14:50:23 +0300

    Add: new entity

---
+++ Entities/Models/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml;
+using Entities.LinkModels;
+
+namespace Entities.Models;
+
+public class Entity
+{
+    private void WriteLinksToXml(string key, object value, XmlWriter writer)
+    {
+        writer.WriteStartElement(key);
+        if (value.GetType() == typeof(List<Link>))
+        {
+            foreach (var val in value as List<Link>)
+            {
+                writer.WriteStartElement(nameof(Link));
+                WriteLinksToXml(nameof(val.Href), val.Href, writer);
+                WriteLinksToXml(nameof(val.Method), val.Method, writer);
+                WriteLinksToXml(nameof(val.Rel), val.Rel, writer);
+                writer.WriteEndElement();
+            }
+        }
+        else
+        {
+            writer.WriteString(value.ToString());
+        }
+        writer.WriteEndElement();
+    }
+}
