16e97d8eeda9faf8f9d123e2fb4678e9a6221e60
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-21 16:20:41 +0300

    Authentication service is used in the controller

---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.ActionFilters;
+using Service.Contracts;
+using Shared.DataTransferObject;
+
+namespace Presentation.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public AuthenticationController(IServiceManager service) => _service = service;
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+    {
+        var result = await _service.AuthenticationService.RegisterUser(userForRegistration);
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        return StatusCode(201);
+    }
+}
