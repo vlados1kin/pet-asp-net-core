1ebf1b943015e7337f720678c974756e993bd96c
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-21 15:31:14 +0300

    Created new migration

---
+++ CompanyEmployees/Migrations/20240721122853_AddedRolesToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UltimateASPNETApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRolesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2567a2f4-8216-4391-b1fd-53fbf1df64af", null, "Administrator", "ADMINISTRATOR" },
+                    { "393d6302-43ab-4134-9538-cd33270a7c67", null, "Manager", "MANAGER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2567a2f4-8216-4391-b1fd-53fbf1df64af");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "393d6302-43ab-4134-9538-cd33270a7c67");
+        }
+    }
+}
