4544b3bd5c03c63aa39306a26a8e79be5d08f292
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-22 12:29:27 +0300

    Add endpoint for authentication

--- Presentation/Controllers/AuthenticationController.cs
+++ Presentation/Controllers/AuthenticationController.cs
@@ -30,4 +30,13 @@ public class AuthenticationController : ControllerBase
 
         return StatusCode(201);
     }
+
+    [HttpPost("login")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> Authenticate([FromBody] UserForAuthenticationDto user)
+    {
+        if (!await _service.AuthenticationService.ValidateUser(user))
+            return Unauthorized();
+        return Ok(new { Token = await _service.AuthenticationService.CreateToken() });
+    }
 }
