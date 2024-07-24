b65686969a36ef4dde9351e60b6635c0ab91ba62
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-25 00:08:11 +0300

    Set version to controllers

--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -11,6 +11,7 @@ namespace Presentation.Controllers;
 [ApiVersion("1.0")]
 [Route("api/companies")]
 [ApiController]
+[ApiExplorerSettings(GroupName = "v1")]
 public class CompaniesController : ControllerBase
 {
     private readonly IServiceManager _service;
