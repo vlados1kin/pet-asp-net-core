59d3136f4757e77befd8b74af1ff44dce44efad3
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-25 00:43:12 +0300

    Use triple-slash comment for create action

--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -25,6 +25,10 @@ public class CompaniesController : ControllerBase
         return Ok();
     }
 
+    /// <summary>
+    /// Get the list of all companies
+    /// </summary>
+    /// <returns>The companies list</returns>
     [HttpGet(Name = "GetCompanies")]
     [Authorize(Roles = "Manager")]
     public async Task<IActionResult> GetCompanies()
@@ -43,7 +47,18 @@ public class CompaniesController : ControllerBase
         return Ok(company);
     }
 
+    /// <summary>
+    /// Creates a newly created company
+    /// </summary>
+    /// <param name="company"></param>
+    /// <returns>A newly created company</returns>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="422">If the model is invalid</response>
     [HttpPost(Name = "CreateCompany")]
+    [ProducesResponseType(201)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(422)]
     [ServiceFilter(typeof(ValidationFilterAttribute))]
     public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto company)
     {
