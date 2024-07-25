9df7b2c4d649beba8d3592ee4cf88f2121d9b877
Vladislav Lashkin <lashkin2004@gmail.com>
2024-07-25 23:12:16 +0300

    Create custom api response classes

---
+++ Entities/Responses/ApiBaseResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Responses;
+
+public abstract class ApiBaseResponse
+{
+    public bool Success { get; set; }
+    protected ApiBaseResponse(bool success) => Success = success;
+}
